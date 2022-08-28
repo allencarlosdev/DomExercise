3a6111ad2ef778652f35d23b6b0a3f66943d61b1
Carlos Allen <allencarlosDev@gmail.com>
2022-08-27 23:06:49 -0400

    Scroll Btn Exercise Dom 7

---
+++ js/index_dom5.js
@@ -0,0 +1,19 @@
+import hamburgerMenu from "./menu_hamburger.js";
+import {digitalClock, alarm} from "./clock.js";
+import {moveBall, shortCuts}from "./keyboard.js";
+import countdown from "./countdown.js";
+import scrollTopButton from "./btn_scroll.js";
+
+const d = document;
+d.addEventListener("DOMContentLoaded", (e)=>{
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#clock", "#activate-clock", "#deactivated-clock");
+    alarm("../assets/alarma_2.mp3","#activate-alarm", "#deactivated-alarm")
+    countdown("countdown", "2022-10-24 00:00:00", " Happy Birthday Carlos Allen");
+    scrollTopButton(".scroll-top-btn");
+});
+
+d.addEventListener("keydown", e =>{
+    shortCuts(e);
+    moveBall(e,".ball", ".stage")
+})
